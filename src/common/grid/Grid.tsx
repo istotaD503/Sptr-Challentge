30f1559101735b70cc26c144d4c73aff7985398a
Alex Mazhavy <alex.mazhavy@gsocap.com>
2020-11-05 23:17:38 -0500

    added no results plugin

--- src/common/grid/Grid.tsx
+++ src/common/grid/Grid.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { GridProps } from "../../interfaces/Restaurant";
 import { HeaderRow } from "./HeaderRow";
 import { DataRow } from "./DataRow";
+import { NoResultsPlug } from "./NoResultsPlug";
 
 export class Grid<T extends string> extends React.Component<GridProps<T>, never> {
 
@@ -11,12 +12,20 @@ export class Grid<T extends string> extends React.Component<GridProps<T>, never>
 
   render() {
     const { data, colDefs, globalSearch } = this.props;
+    const filtered = data.filter(globalSearch);
     return (
         <div className="Grid">
             <HeaderRow colDefs={colDefs}></HeaderRow>
-            {data.filter(globalSearch).map((rest: any) => {
-                return <DataRow key={rest.id} record={rest} colDefs={colDefs}></DataRow>
-            })}
+            {
+                !!filtered && !!filtered.length ?
+                <div>       
+                    {filtered.map(rest => {
+                        return <DataRow key={rest.id} record={rest} colDefs={colDefs}></DataRow>
+                    })}
+                </div> :
+                <NoResultsPlug></NoResultsPlug>
+            }
+
         </div>
         );
   }
---
+++ src/common/grid/NoResultsPlug.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export const NoResultsPlug = () => 
+<div className="NoResultsPlug">
+    No Results Found
+</div>
